501d8566f9f6dd7e7be6e4ca8abddfe384106b99
VCM admin account <vcm@vcm-30918.vm.duke.edu>
2023-09-07 00:39:23 -0400

    change main to a Pandas Descriptive Statistics Script with test

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def add(a, b):
-    return a + b
+import pandas as pd
+
+def load_data():
+    return pd.read_csv("dataset_sample.csv")
+
+def get_descriptive_statistics(data):
+    return data.describe()
+
+def main():
+    data = load_data()
+    stats = get_descriptive_statistics(data)
+    print(stats)
 
 if __name__ == "__main__":
-    print(add(2, 3))
+    main()
--- test_main.py
+++ test_main.py
@@ -1,4 +1,28 @@
 import main
+import pandas as pd
 
-def test_add():
-    assert main.add(2, 3) == 5
+def test_load_data():
+    data = main.load_data()
+    assert isinstance(data, pd.DataFrame)
+    assert not data.empty
+
+def test_get_descriptive_statistics():
+    data = main.load_data()
+    stats = main.get_descriptive_statistics(data)
+
+    # We know the dataset_sample.csv has 'Product', 'Price', and 'Quantity' columns.
+    # We'll make some basic assertions based on these.
+
+    assert "Product" in stats.index
+    assert "Price" in stats.columns
+    assert "Quantity" in stats.columns
+
+    # Check some basic stats
+    assert stats.loc["count", "Price"] == 9
+    assert stats.loc["count", "Quantity"] == 9
+    assert stats.loc["mean", "Price"] == (1.2 + 0.5 + 2.5 + 1.3 + 0.6 + 2.4 + 1.1 + 0.7 + 2.6) / 9
+    assert stats.loc["mean", "Quantity"] == (50 + 40 + 20 + 45 + 50 + 22 + 55 + 42 + 19) / 9
+    assert stats.loc["min", "Price"] == 0.5
+    assert stats.loc["min", "Quantity"] == 19
+    assert stats.loc["max", "Price"] == 2.6
+    assert stats.loc["max", "Quantity"] == 55
