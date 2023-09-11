6d91dbb2cbd7c59f75181519f91013520417da39
VCM admin account <vcm@vcm-30918.vm.duke.edu>
2023-09-10 22:25:26 -0400

    add a data visualization

--- test_main.py
+++ test_main.py
@@ -3,19 +3,24 @@ import pandas as pd
 
 def test_load_data():
     data = main.load_data()
+    # Check if the data is loaded as DataFrame and is not empty
     assert isinstance(data, pd.DataFrame)
     assert not data.empty
+
+    # Check if known columns 'Product', 'Price', and 'Quantity' are present
+    for col in ["Product", "Price", "Quantity"]:
+        assert col in data.columns
 
 def test_get_descriptive_statistics():
     data = main.load_data()
     stats = main.get_descriptive_statistics(data)
 
-    # Check if 'Product', 'Price', and 'Quantity' are in the columns
-    assert "Product" not in stats.columns  # 'Product' shouldn't be in stats since it's non-numeric
-    assert "Price" in stats.columns
-    assert "Quantity" in stats.columns
+    # Ensure 'Product' column isn't in the statistics
+    assert "Product" not in stats.columns
 
-    # Check if the stats such as 'count', 'mean', 'std', etc., are in the index.
-    for stat in ["count", "mean", "std", "min", "25%", "50%", "75%", "max"]:
-        assert stat in stats.index
+    # For numerical columns, check if statistical indices are present
+    for col in ["Price", "Quantity"]:
+        assert col in stats.columns
+        for stat in ["count", "mean", "std", "min", "25%", "50%", "75%", "max"]:
+            assert stat in stats.index
 
